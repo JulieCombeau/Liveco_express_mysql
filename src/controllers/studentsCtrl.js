c61cff373ff18441884c351713b0ff37d5151c39
Loris <45939615+lchastanet@users.noreply.github.com>
2022-04-21 16:53:16 +0200

    add findOneById

--- src/controllers/studentsCtrl.js
+++ src/controllers/studentsCtrl.js
@@ -6,3 +6,18 @@ exports.getAll = (req, res) => {
     .then((datas) => res.send(datas))
     .catch((err) => res.status(500).send(err))
 }
+
+exports.getOneById = (req, res) => {
+  const id = parseInt(req.params.id)
+
+  studentsModel
+    .findOneById(id)
+    .then((datas) => {
+      if (datas.length === 0) {
+        res.status(404).send({ error: "Nothing found !" })
+      } else {
+        res.send(datas)
+      }
+    })
+    .catch((err) => res.status(500).send(err))
+}
--- src/routes/studentsRoutes.js
+++ src/routes/studentsRoutes.js
@@ -4,5 +4,6 @@ const studentsCtrl = require("../controllers/studentsCtrl")
 const router = express.Router()
 
 router.get("/", studentsCtrl.getAll)
+router.get("/:id", studentsCtrl.getOneById)
 
 module.exports = router
